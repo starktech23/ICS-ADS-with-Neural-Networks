1b1cc740860281d179e7879355d667589ed2608f
Aditya Srivastava <adiyash96@gmail.com>
2017-06-14 04:20:56 +0530

    Add files via upload

---
+++ VM-OPC/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#RTU Server
+from config import *
+from pymodbus.server.sync import StartTcpServer
+
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+import logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+store = ModbusSlaveContext(di = ModbusSequentialDataBlock(0, [0]*100))
+context = ModbusServerContext(slaves=store, single=True)
+identity = ModbusDeviceIdentification()
+
+StartTcpServer(context, identity=identity, address=(FIELD_IP, FIELD_PORT)
